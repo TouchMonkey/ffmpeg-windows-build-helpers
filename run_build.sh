d5cccf484b1bb113230e1a5778a4b2f2d5c0932e
Trevor <trevortheduke@gmail.com>
2019-12-16 15:21:09 -0500

    Script cleanup.

--- run_build.sh
+++ run_build.sh
@@ -2,17 +2,14 @@
 
 #https://github.com/rdp/ffmpeg-windows-build-helpers
 
-SCRIPT_ARGS=""
+SCRIPT_ARGS="./cross_compile_ffmpeg.sh"
 
 SCRIPT_ARGS="${SCRIPT_ARGS} --ffmpeg-git-checkout=https://github.com/TouchMonkey/FFmpeg.git"
 SCRIPT_ARGS="${SCRIPT_ARGS} --ffmpeg-git-checkout-version=vidstab_full_range"
 
 SCRIPT_ARGS="${SCRIPT_ARGS} --build-ffmpeg-static=y"
 
-#SCRIPT_ARGS="${SCRIPT_ARGS} --compiler-flavors=win64,win32"
 SCRIPT_ARGS="${SCRIPT_ARGS} --compiler-flavors=win64"
-
-#SCRIPT_ARGS="${SCRIPT_ARGS} --build-x264-with-libav=y"
 
 SCRIPT_ARGS="${SCRIPT_ARGS} --git-get-latest=y"
 
@@ -20,17 +17,16 @@ SCRIPT_ARGS="${SCRIPT_ARGS} --enable-gpl=y"
 
 SCRIPT_ARGS="${SCRIPT_ARGS} --disable-nonfree=n"
 
-#SCRIPT_ARGS="${SCRIPT_ARGS} --prefer-stable=n"
 SCRIPT_ARGS="${SCRIPT_ARGS} --prefer-stable=y"
 
 SCRIPT_ARGS="${SCRIPT_ARGS} --gcc-cpu-count=4"
 
-#SCRIPT_ARGS="${SCRIPT_ARGS} --cflags=\"-mtune=generic -O2\""
-SCRIPT_ARGS="${SCRIPT_ARGS} --cflags=\"-march=native -O2\""
+#SCRIPT_ARGS="${SCRIPT_ARGS} \"--cflags='-march=native -O2'\""
 
 
-echo ./cross_compile_ffmpeg.sh ${SCRIPT_ARGS} "$@"
-./cross_compile_ffmpeg.sh ${SCRIPT_ARGS} "$@"
+echo ${SCRIPT_ARGS} "$@"
+#${SCRIPT_ARGS} "$@"
+${SCRIPT_ARGS} --cflags='-march=native -O2' "$@"
 retval=$?
 if [ ${retval} -ne 0 ]; then
 	echo "ERROR: Build failed with code $retval"
@@ -41,8 +37,9 @@ fi
 SCRIPT_ARGS="${SCRIPT_ARGS} --high-bitdepth=y"
 
 
-echo ./cross_compile_ffmpeg.sh ${SCRIPT_ARGS} "$@"
-./cross_compile_ffmpeg.sh ${SCRIPT_ARGS} "$@"
+echo {SCRIPT_ARGS} "$@"
+#${SCRIPT_ARGS} "$@"
+${SCRIPT_ARGS} --cflags='-march=native -O2' "$@"
 retval=$?
 if [ ${retval} -ne 0 ]; then
 	echo "ERROR: Build failed with code $retval"
